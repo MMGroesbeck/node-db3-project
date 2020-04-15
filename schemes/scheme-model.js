1c1e6d6d6d17aa381d530c4ffc3762f98071ec52
M Groesbeck <m.groesbeck@gmail.com>
2020-04-15 15:30:12 -0700

    Updated return behavior for delete endpoint.

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,42 +1,61 @@
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
 
 module.exports = {
-    find,
-    findById,
-    findSteps,
-    add,
-    update,
-    remove
-}
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+};
 
 function find() {
-    return db('schemes');
+  return db("schemes");
 }
 
 function findById(id) {
-    return db('schemes').where({ id: id });
+  return db("schemes").where({ id: id });
 }
 
 function findSteps(id) {
-    return db
-    .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
-    .from('steps')
-    .innerJoin('schemes', 'steps.scheme_id', 'schemes.id')
+  return db
+    .select(
+      "steps.id",
+      "schemes.scheme_name",
+      "steps.step_number",
+      "steps.instructions"
+    )
+    .from("steps")
+    .innerJoin("schemes", "steps.scheme_id", "schemes.id")
     .where({ scheme_id: id })
-    .orderBy('steps.step_number');
+    .orderBy("steps.step_number");
 }
 
 function add(scheme) {
-    return db('schemes').insert(scheme, "id")
-        .then(([id]) => {
-            return findById(id);
-        });
+  return db("schemes")
+    .insert(scheme, "id")
+    .then(([id]) => {
+      return findById(id);
+    });
 }
 
 function update(changes, id) {
-    return db('schemes').where({ id }).update(changes);
+  return db("schemes")
+    .where({ id })
+    .update(changes)
+    .then(([i]) => {
+      return findById(i);
+    });
 }
 
 function remove(id) {
-    return db('schemes').where({ id }).del();
+    return findById(id)
+        .then(schemes => {
+            return db("schemes")
+                .where({ id })
+                .del()
+                .then(count => {
+                    return schemes[0];
+                })
+        })
 }
